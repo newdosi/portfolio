96693d714a5f3a4756029e50e71bd1224b5b07c7
newdosi <new.dosi.design@gmail.com>
2025-01-31 16:10:37 +0900

    upload

--- src/components/animation/LottieImage.tsx
+++ src/components/animation/LottieImage.tsx
@@ -1,12 +1,17 @@
 "use client";
 
 import React from "react";
-import Lottie from "react-lottie-player";
+import dynamic from "next/dynamic";
+
+// 클라이언트 사이드에서만 로딩하도록 설정
+const LottiePlayer = dynamic(() => import("react-lottie-player"), {
+  ssr: false,
+});
 import animationData from "../../../public/animation.json";
 
 export default function LottieImage() {
   return (
-    <Lottie
+    <LottiePlayer
       loop
       animationData={animationData}
       play
